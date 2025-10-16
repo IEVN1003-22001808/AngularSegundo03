80e81f1991e98fe0883ac78a725b96fbc136bf9f
IEVN1003-22001808 <georgeizac.skul@gmail.com>
2025-10-15 18:56:35 -0600

    Rutas 15/10/2025

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DistanciaComponent } from './formulario/distancia/distancia.component';
 import { ZodiacoComponent } from './formulario/zodiaco/zodiaco.component';
 import { AxbComponent } from './formulario/axb/axb.component';
 
+import { initFlowbite } from 'flowbite';
+import { NavbarComponent } from './navbar/navbar.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DistanciaComponent, ZodiacoComponent, AxbComponent],
+  imports: [RouterOutlet, DistanciaComponent, ZodiacoComponent, AxbComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'AngularSegundo03';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'Auth', loadChildren: () => import ('./auth/features/auth.routes').then((papu) => papu.default) },
+
+    { path: 'Formulario', loadChildren: () => import ('./formulario/formulario.routes').then((papu) => papu.default) },
+
+    { path: 'UTL', loadChildren: () => import ('./utl/utl.routes').then((papu) => papu.default) },
+
+];
